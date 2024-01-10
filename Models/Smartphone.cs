5a44fa6b7b342eacb9b5e8cd74b117f821f80b68
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-01-10 20:36:14 -0300

    feat: desafio concluido, implementado também o método Informacoes, onde mostra as infomações do Smartphone criado.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,16 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -21,6 +26,17 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
+        public void Informacoes(string marca)
+        {
+            Console.WriteLine("________________________________________");
+            Console.WriteLine($"Informações sobre o Smartphone {marca}");
+            Console.WriteLine($"Modelo:  {Modelo}.");
+            Console.WriteLine($"Número:  {Numero}.");
+            Console.WriteLine($"IMEI:    {IMEI}.");
+            Console.WriteLine($"Memória: {Memoria}Gb.");
+            Console.WriteLine("----------------------------------------");
+
+        }
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone Iphone = new Iphone("11999999999", "Modelo 14", "IMEI 896", 256);
+Iphone.Ligar();
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Whatsapp");
+Iphone.Informacoes("Iphone");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone Nokia = new Nokia("11988888888", "Modelo 1", "IMEI 1", 64);
+Nokia.Ligar();
+Nokia.ReceberLigacao();
+Nokia.InstalarAplicativo("Facebook");
+Nokia.Informacoes("Nokia");
